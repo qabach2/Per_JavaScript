10106f4861e73eff77f82746706f1e2b4397367f
MAHLET HAILU <62271023+qabach2@users.noreply.github.com>
2024-04-24 08:48:40 -0400

    Function start

--- Javascript_projects/Practice_Loop/script.js
+++ Javascript_projects/Practice_Loop/script.js
@@ -1,3 +1,7 @@
+// Fizz Buzz loop example number 1 from 100 if the number is divisible by
+//  3 it will print "Fizz" or if it is divisible by 5 it will print "buzz"
+//  or If the number is divisible by both 3 and 5 it will print "fizz Buzz"
+//  or if neither It will print "Done"
 for( let i=0; i<100; i++){
     if(i % 3==0){
         console.log("Fizz");
@@ -15,3 +19,14 @@ for( let i=0; i<100; i++){
     }
 
 }
+// Prime Time Excercise
+let num=1
+while(j<=100){
+    if(j%3==0){
+        console.log(j)
+    }else {
+        console.log("not prime")
+    }
+}
+
+
